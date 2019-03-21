82853d566d3576003b6a235fe44ad03521da4f37
ct <ctaylr13@gmail.com>
2019-03-21 12:57:03 -0400

    Create twosum.js

---
+++ twosum.js
@@ -0,0 +1,21 @@
+var holder = [];
+
+var twoSum = function(nums, target) {
+    for(var i = 0; i < nums.length; i++) {
+        for(var j = i + 1; j < nums.length; j++) {
+            if(nums[j] == target - nums[i]) {      
+                return([i, j]);
+            }
+        }
+    }
+    
+
+    
+};
+
+
+
+var nums = [2, 11, 15, 7];
+var target = 9;
+
+twoSum(nums, target);
